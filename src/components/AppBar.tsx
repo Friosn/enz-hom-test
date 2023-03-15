45a45672122d7bff320d664480371e0ddcf43a0f
Friosn <franz.rios.nino@gmail.com>
2023-03-15 19:14:29 +0100

    StyleSheets in tab and app bars

--- src/components/AppBar.tsx
+++ src/components/AppBar.tsx
@@ -1,22 +1,25 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
 import theme from '../theme';
 
 const AppBar = () => {
   return (
-    <View
-      style={{
-        flex: 1,
-        height: 50,
-        width: '100%',
-        position: 'absolute',
-        top: 0,
-        justifyContent: 'center',
-        backgroundColor: theme.colors.backgroundC,
-      }}>
+    <View style={styles.container}>
       <Text style={{textAlign: 'center'}}>AppBar</Text>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    height: 55,
+    width: '100%',
+    position: 'absolute',
+    top: 0,
+    justifyContent: 'center',
+    backgroundColor: theme.colors.backgroundC,
+  },
+});
 
 export default AppBar;
--- src/components/TabBar.tsx
+++ src/components/TabBar.tsx
@@ -1,22 +1,25 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
 import theme from '../theme';
 
 const TabBar = () => {
   return (
-    <View
-      style={{
-        flex: 1,
-        height: 50,
-        width: '100%',
-        position: 'absolute',
-        bottom: 0,
-        justifyContent: 'center',
-        backgroundColor: theme.colors.backgroundC,
-      }}>
+    <View style={styles.container}>
       <Text style={{textAlign: 'center'}}>TabBar</Text>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    height: 55,
+    width: '100%',
+    position: 'absolute',
+    bottom: 0,
+    justifyContent: 'center',
+    backgroundColor: theme.colors.backgroundC,
+  },
+});
 
 export default TabBar;
